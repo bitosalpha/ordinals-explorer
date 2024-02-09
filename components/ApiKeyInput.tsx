9172a11cddcf4529a9210e9d12f1d006144222b8
bitosalpha <brandonyuen1998@gmail.com>
2024-02-09 20:44:01 +0800

    main

--- components/ApiKeyInput.tsx
+++ components/ApiKeyInput.tsx
@@ -1,28 +1,32 @@
-import {Card, Input} from "antd";
+import { Card, Input } from "antd";
 import React from "react";
-import {setApiKey} from "../utils/httpUtils";
 
-export function ApiKeyInput({apiKey,setKey}:{
-    apiKey: string |undefined,
+export function ApiKeyInput({
+    apiKey,
+    setKey
+}: {
+    apiKey: string | undefined,
     setKey: (apiKey: string) => void
 }) {
 
-    return <Card size={'small'}>
-        <div className={'flex-row-v-center gap-16 bold'}>
-            Set API key:
-            <div className="flex-1">
-                <div className="api-key-input">
-                    <Input type="text"
-                           placeholder="Enter API key"
-                           value={apiKey}
-                           onChange={(e) => {
-                               const key = e.target.value;
-                               setKey(key);
-                               setApiKey(key);
-                           }}
-                    />
+    return (
+        <Card size={'small'}>
+            <div className={'flex-row-v-center gap-16 bold'}>
+                Set API key:
+                <div className="flex-1">
+                    <div className="api-key-input">
+                        <Input 
+                            type="text"
+                            placeholder="Enter API key"
+                            value={apiKey || ''}
+                            onChange={(e) => {
+                                const key = e.target.value;
+                                setKey(key);
+                            }}
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
-    </Card>
+        </Card>
+    );
 }
